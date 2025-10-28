49c4e8c073158d7ed759ac6ab20df588fd71cedf
Nabin Tiwari <nabintywari111@gmail.com>
2025-10-28 12:42:10 +0530

    Add MatchesController with basic endpoints
    Introduce a new MatchesController in the OurRestAPI.Controllers
    namespace. This controller includes two HTTP GET endpoints:
    - The Index method at route `api/matches` returns a JSON message
      confirming the controller is operational.
    - The Players method at route `/players` returns a JSON array of
      popular football players.
    Both methods use the Ok method to return HTTP 200 responses.

---
+++ OurRestAPI/Controllers/MatchesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class MatchesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok( new {message="Hello, Matches Controller is Working"});
+        }
+        [HttpGet("/players")]
+        public IActionResult Players()
+        {
+            List<string> footballPlayers = new List<string>
+        {
+            "Lionel Messi",
+            "Cristiano Ronaldo",
+            "Neymar Jr",
+            "Kylian Mbappe",
+            "Kevin De Bruyne",
+            "Robert Lewandowski",
+            "Mohamed Salah",
+            "Sadio Mane",
+            "Virgil van Dijk",
+            "Harry Kane",
+            "Erling Haaland",
+            "Karim Benzema",
+            "Luka Modric",
+            "Sergio Ramos",
+            "Paul Pogba",
+            "Raheem Sterling",
+            "Jadon Sancho",
+            "Eden Hazard",
+            "Toni Kroos",
+            "Manuel Neuer"
+        };
+            return Ok( footballPlayers);
+        }
+    }
+}
